fe16595a538bbbaa3c0e6ad53bbaa42c8fb05b87
Kominami-Hisashi <komi73tov@gmail.com>
2024-02-12 23:59:46 +0900

    Finished User Test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,8 +19,10 @@ class ActiveSupport::TestCase
   #
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  #あくてぃぶなユーザーを返す
+  def active_user
+    User.find_by(activated: true)
+  end
 
   # Add more helper methods to be used by all tests here...
 end
